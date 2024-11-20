128d93a1283eba1724357cc8ad6a35d5620905c6
camoreno368 <camoreno368@hotmail.com>
2024-11-19 21:07:28 -0500

    insert Gradio

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
+import gradio as gr
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 
 class HuggingFaceModel:
-    def __init__(self, model_name="jcortizba/modelo18"):
+    def __init__(self, model_name="jaortizb/modelo18"):
         self.model_name = model_name
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
         self.model = AutoModelForSequenceClassification.from_pretrained(self.model_name)
@@ -13,8 +14,28 @@ class HuggingFaceModel:
         probabilities = torch.nn.functional.softmax(outputs.logits, dim=-1)
         return probabilities.tolist()
 
-if __name__ == "__main__":
+# Función para la interfaz
+def predict_with_model(input_text):
     model = HuggingFaceModel()
-    text = "Este es un ejemplo de entrada."
-    predictions = model.predict(text)
-    print(f"Predicciones: {predictions}")
+    predictions = model.predict(input_text)
+    return predictions
+
+# Definir interfaz en Gradio
+def main():
+    with gr.Blocks() as demo:
+        gr.Markdown("# Modelo Hugging Face en Gradio")
+        input_text = gr.Textbox(label="Ingrese texto para predecir")
+        output_label = gr.Label(label="Predicción")
+        predict_button = gr.Button("Predecir")
+        
+        predict_button.click(
+            fn=predict_with_model,
+            inputs=[input_text],
+            outputs=[output_label],
+        )
+
+    demo.launch()
+
+if __name__ == "__main__":
+    main()
+
--- test/test.py
+++ test/test.py
@@ -6,13 +6,23 @@ class TestHuggingFaceModel(unittest.TestCase):
         self.model = HuggingFaceModel()
 
     def test_predict_output(self):
-        text = "Prueba de entrada."
+        text = "un arbol corto las cuerdas"
         predictions = self.model.predict(text)
         self.assertIsInstance(predictions, list)
         self.assertGreater(len(predictions), 0)
         for prob in predictions[0]:
             self.assertGreaterEqual(prob, 0)
             self.assertLessEqual(prob, 1)
+    
+class TestGradioInterface(unittest.TestCase):
+    def test_predict_with_model(self):
+        input_text = "no hay energia en el sector"
+        predictions = predict_with_model(input_text)
+        self.assertIsInstance(predictions, list)
+        self.assertGreater(len(predictions), 0)
+        for prob in predictions[0]:
+            self.assertGreaterEqual(prob, 0)
+            self.assertLessEqual(prob, 1)   
 
 if __name__ == "__main__":
     unittest.main()
