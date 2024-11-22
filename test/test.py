007b3bdfbe1f1f7ad1dd9a0298299ba5979d50ad
DiegoParraGarces <diego.parra1026@gmail.com>
2024-11-22 09:27:48 -0500

    Modificación test.py

--- test/test.py
+++ test/test.py
@@ -6,23 +6,26 @@ class TestHuggingFaceModel(unittest.TestCase):
         self.model = HuggingFaceModel()
 
     def test_predict_output(self):
+        # Probar con una entrada válida
         text = "un arbol corto las cuerdas"
         predictions = self.model.predict(text)
-        self.assertIsInstance(predictions, list)
-        self.assertGreater(len(predictions), 0)
-        for prob in predictions[0]:
-            self.assertGreaterEqual(prob, 0)
-            self.assertLessEqual(prob, 1)
+        self.assertIsInstance(predictions, str)  # Verificar que la salida es un string
+        self.assertIn(predictions, ["ejecutar", "cancelar"])  # Validar que es una etiqueta válida
     
+    def test_invalid_input(self):
+        # Probar con entrada inválida
+        invalid_text = "12"  # Menos de 3 caracteres o puramente numérico
+        result = self.model.predict(invalid_text)
+        self.assertIsInstance(result, str)
+        self.assertEqual(result, "Entrada inválida. Por favor, ingrese un texto coherente.")
+
 class TestGradioInterface(unittest.TestCase):
     def test_predict_with_model(self):
+        # Probar con texto para la interfaz Gradio
         input_text = "no hay energia en el sector"
         predictions = predict_with_model(input_text)
-        self.assertIsInstance(predictions, list)
-        self.assertGreater(len(predictions), 0)
-        for prob in predictions[0]:
-            self.assertGreaterEqual(prob, 0)
-            self.assertLessEqual(prob, 1)   
+        self.assertIsInstance(predictions, str)  # Verificar que la salida es un string
+        self.assertIn(predictions, ["ejecutar", "cancelar"])  # Validar que es una etiqueta válida
 
 if __name__ == "__main__":
     unittest.main()
